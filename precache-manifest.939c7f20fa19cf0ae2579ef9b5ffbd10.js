f321dd4105ea3c51091fd204ed7f4209dd41b564
Parth Patel <pvpprod89@gmail.com>
2018-10-29 22:00:59 -0400

    Updates

---
+++ precache-manifest.939c7f20fa19cf0ae2579ef9b5ffbd10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "5a9265618bec55e7f2cc",
+    "url": "/clickyGame/static/js/main.5a926561.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/clickyGame/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "5a9265618bec55e7f2cc",
+    "url": "/clickyGame/static/css/main.ea350306.chunk.css"
+  },
+  {
+    "revision": "25b7aa78cd0fb0a322f6e94ec6720f0b",
+    "url": "/clickyGame/index.html"
+  }
+];
